b7e0b5695bd295ce6e1c08bd00e603d6dbcb9179
alessandro-stella <alessandro.stella2004@gmail.com>
2026-09-10 18:16:22 +0200

    Testing syncronization

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: process.env.NODE_ENV === "production" },
+  ssl: { rejectUnauthorized: false },
 
   query_timeout: 10000,
 
